d36dc1fae1b16467bcc904af4b381dc8afe8a90b
alysonbruno <alysonbruno554@gmail.com>
2023-08-21 11:43:27 -0300

    Initial commit

---
+++ lisata051/questao02.py
@@ -0,0 +1,12 @@
+"""
+Desenvolver um programa que exiba os números em ordem decrescente de 100 até 1
+"""
+
+contador = 100
+
+while (contador >= 1):
+    print(f"contador: {contador}")
+    contador = contador - 1
+
+print("Depois do bloco while")
+
